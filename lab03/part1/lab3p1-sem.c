1d0c2b1e48c886c6198a633611ce7f181c35a2e5
e0406479 <e0406479@xcne1>
2022-03-13 03:30:16 +0800

    lab03 part 1 and 2 done

--- lab03/part1/lab3p1-sem.c
+++ lab03/part1/lab3p1-sem.c
@@ -6,18 +6,21 @@
 #include <sys/wait.h>
 
 #define NUM_PROCESSES 5
-
+ 
 int main() {
 
     int i, j, pid;
-    int shmid1, shmid2, shmid3, shmid4;
-    sem_t *sem1, *sem2, *sem3, *sem4;
-
-    shmid1 = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0600);
-    shmid2 = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0600);
-    shmid3 = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0600);
-    shmid4 = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0600);
-
+    int shmid[NUM_PROCESSES];
+    sem_t * sems[NUM_PROCESSES];
+    for (int x = 0; x < NUM_PROCESSES; x++) {
+        shmid[x] = shmget(IPC_PRIVATE, sizeof(sem_t), IPC_CREAT | 0600);
+        sems[x] = (sem_t *) shmat(shmid[x], NULL, 0);
+        if (x == 0) {
+            sem_init(sems[x],1,1);
+        } else {
+            sem_init(sems[x],1,0);
+        }
+    }
 
     for(i=0; i<NUM_PROCESSES; i++)
     {
@@ -27,6 +30,7 @@ int main() {
     }
 
     if(pid == 0) {
+        sem_wait(sems[i]);
         printf("I am child %d\n", i);
 
         for(j = i*10; j<i*10 + 10; j++){
@@ -36,16 +40,16 @@ int main() {
         }
 
         printf("\n\n");
-        //sem_post(sem[i+1]);
+        sem_post(sems[i+1]);
     }
     else {
         for(i=0; i<NUM_PROCESSES; i++) 
             wait(NULL);
-            //sem_destroy(sem1);
-            //sem_destroy(sem2);
-            //sem_destroy(sem3);
-            //sem_destroy(sem4);
-            //shmctl(shmid, IPC_RMID, 0);
+        for(int x = 0; x < NUM_PROCESSES; x++) {
+            sem_destroy(sems[x]);
+            shmdt(sems[x]);
+            shmctl(shmid[x], IPC_RMID, 0);
+        }
     }
 
 }
