493c2b87ed71db9b05a40b95ae359bee0a9f3626
e0406479 <e0406479@xcne1>
2022-03-13 13:43:29 +0800

    lab03 done

--- lab03/part3/bigsmall-par.c
+++ lab03/part3/bigsmall-par.c
@@ -5,6 +5,8 @@
 #include <time.h>
 #include <sys/wait.h>
 #include "config.h"
+#include <sys/shm.h>
+#include "barrier.h"
 
 #define NUM_PROCESSES  8
 
@@ -12,7 +14,7 @@ int main() {
 
     int vect[VECT_SIZE];
     int pid;
-    int largest[NUM_PROCESSES], smallest[NUM_PROCESSES];
+    int *largest, *smallest;
 
     float per_process_raw = (float) VECT_SIZE / NUM_PROCESSES;
     int per_process = (int) per_process_raw;
@@ -25,8 +27,15 @@ int main() {
         exit(-1);
     }
 
+    init_barrier(NUM_PROCESSES+1);
     srand(24601);
-    int i;
+    int i, shmid1, shmid2;
+
+    // Creating shared memory for largest and smallest
+    shmid1 = shmget(IPC_PRIVATE, sizeof(sizeof(int) * NUM_PROCESSES), IPC_CREAT | 0600);
+    largest = (int *) shmat(shmid1, NULL, 0);
+    shmid2 = shmget(IPC_PRIVATE, sizeof(sizeof(int) * NUM_PROCESSES), IPC_CREAT | 0600);
+    smallest = (int *) shmat(shmid2, NULL, 0);
 
     for(i=0; i<VECT_SIZE; i++) {
         vect[i] = rand();
@@ -55,13 +64,15 @@ int main() {
             if(vect[j] < small)
                 small = vect[j];
         }
+
         largest[i] = big;
         smallest[i] = small;
-
+        reach_barrier();
     }
     else 
     {
         start = clock();
+        reach_barrier();
         for(j=0; j<NUM_PROCESSES; j++)
         {
             if(largest[j] > big)
@@ -81,6 +92,12 @@ int main() {
         // Clean up process table
         for(j=0; j<NUM_PROCESSES; j++)
             wait(NULL);
+
+        destroy_barrier(pid);
+        shmdt(largest);
+        shmdt(smallest);
+        shmctl(shmid1, IPC_RMID,0);
+        shmctl(shmid2, IPC_RMID,0);
     }
 }
 
